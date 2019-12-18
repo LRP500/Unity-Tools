ce1da7010a528c0663519f2c82cc196dc250182c
pierre.roy <pierre.roy@epitech.eu>
2019-12-18 21:39:25 +0100

    assembly definitions + minor changes

--- Collections/Lists/SelectableList.cs
+++ Collections/Lists/SelectableList.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace TopDownShooter
+namespace TopDownStealth
 {
     public abstract class SelectableList<T> : ScriptableObject
     {
---
+++ Utilities/Misc/EditorOnly.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Tools.Editor
+{
+    public class EditorOnly : MonoBehaviour
+    {
+        public bool disableOnPlay = true;
+
+        private void Awake()
+        {
+#if !UNITY_EDITOR
+       Destroy(this.gameObject);
+#else
+            gameObject.SetActive(!disableOnPlay);
+#endif
+        }
+    }
+}
