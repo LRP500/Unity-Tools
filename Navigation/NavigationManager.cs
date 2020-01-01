7707df3ffd0ce128379f0494039fd4be524e94be
pierre.roy <pierre.roy@epitech.eu>
2020-01-01 21:30:48 +0100

    navigation manager rework

--- Navigation/NavigationManager.cs
+++ Navigation/NavigationManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,16 +8,50 @@ namespace Tools.Navigation
     [CreateAssetMenu(menuName = "Tools/Navigation/Navigation Manager")]
     public class NavigationManager : SingletonScriptableObject<NavigationManager>
     {
-        public IEnumerator LoadScene(SceneReference scene)
+        [SerializeField]
+        private SceneReference _master = null;
+
+        public IEnumerator SwitchScenes(SceneReference scene)
         {
-            yield return SceneManager.LoadSceneAsync(scene.path, LoadSceneMode.Single);
+            List<Scene> exceptions = new List<Scene>
+            {
+                SceneManager.GetSceneByPath(_master.path),
+                SceneManager.GetSceneByPath(scene.path)
+            };
+
+            yield return LoadScene(scene);
+            yield return UnloadAllScenes(exceptions);
         }
 
-        public IEnumerator LoadSceneAdditive(SceneReference scene)
+        public IEnumerator LoadScene(SceneReference scene)
         {
             yield return SceneManager.LoadSceneAsync(scene.path, LoadSceneMode.Additive);
 
             SceneManager.SetActiveScene(SceneManager.GetSceneByPath(scene.path));
+        }
+
+        private Scene[] GetLoadedScenes()
+        {
+            int sceneCount = SceneManager.sceneCount;
+            Scene[] loadedScenes = new Scene[sceneCount];
+
+            for (int i = 0, length = SceneManager.sceneCount; i < length; i++)
+            {
+                loadedScenes[i] = SceneManager.GetSceneAt(i);
+            }
+
+            return loadedScenes;
+        }
+
+        private IEnumerator UnloadAllScenes(List<Scene> exceptions = null)
+        {
+            foreach (Scene loadedScene in GetLoadedScenes())
+            {
+                if (exceptions != null && !exceptions.Contains(loadedScene))
+                {
+                    yield return SceneManager.UnloadSceneAsync(loadedScene);
+                }
+            }
         }
 
         public void QuitGame()
