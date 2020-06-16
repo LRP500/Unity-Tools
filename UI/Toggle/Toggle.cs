e5076d0863b293994b2f85c45c97b7bedf8fa0b8
Pierre Roy <lrp.roy@gmail.com>
2020-06-16 22:41:44 +0200

    toggle + toggle group

---
+++ UI/Toggle/Toggle.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace Tools.UI
+{
+    public class Toggle : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+    {
+        [SerializeField]
+        private Animator _animator = null;
+
+        [SerializeField]
+        private UnityEvent _onSelect = null;
+        public UnityEvent OnSelect => _onSelect;
+
+        public ToggleGroup ToggleGroup { get; set; } = null;
+
+        public bool IsSelected { get; private set; } = false;
+        public bool IsDisabled { get; private set; } = false;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (IsDisabled)
+            {
+                return;
+            }
+
+            if (IsSelected)
+            {
+                OnDeselect();
+            }
+            else
+            {
+                _onSelect?.Invoke();
+                SetSelected(true);
+            }
+        }
+
+        public void OnDeselect()
+        {
+            if (!IsDisabled)
+            {
+                return;
+            }
+
+            SetSelected(false);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (IsDisabled)
+            {
+                return;
+            }
+
+            if (!IsSelected)
+            {
+                _animator.SetTrigger("Highlighted");
+            }
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (IsDisabled)
+            {
+                return;
+            }
+
+            if (!IsSelected)
+            {
+                _animator.SetTrigger("Normal");
+            }
+        }
+
+        public void SetSelected(bool value)
+        {
+            if (IsDisabled)
+            {
+                return;
+            }
+
+            IsSelected = value;
+
+            if (IsSelected == true)
+            {
+                if (ToggleGroup)
+                {
+                    ToggleGroup.SetCurrentlySelected(this);
+                }
+
+                _animator.SetTrigger("Selected");
+            }
+            else
+            {
+                _animator.SetTrigger("Normal");
+            }
+        }
+
+        public void SetDisabled(bool value)
+        {
+            IsDisabled = value;
+
+            if (IsDisabled == true)
+            {
+                IsSelected = false;
+                _animator.SetTrigger("Disabled");
+            }
+            else
+            {
+                _animator.SetTrigger("Normal");
+            }
+        }
+    }
+}
---
+++ UI/Toggle/ToggleGroup.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Tools.UI
+{
+    public class ToggleGroup : MonoBehaviour
+    {
+        [SerializeField]
+        private Toggle _firstSelected = null;
+
+        private Toggle _lastSelectedToggle = null;
+        private Toggle _currentSelectedToggle = null;
+
+        private List<Toggle> _toggles = null;
+
+        private void Start()
+        {
+            _toggles = GetComponentsInChildren<Toggle>().ToList();
+
+            for (int i = 0; i < _toggles.Count; i++)
+            {
+                _toggles[i].ToggleGroup = this;
+            }
+
+            if (_toggles.Contains(_firstSelected))
+            {
+                _firstSelected.SetSelected(true);
+            }
+        }
+
+        public void SetCurrentlySelected(Toggle value)
+        {
+            if (_currentSelectedToggle != null)
+            {
+                if (_currentSelectedToggle != value)
+                {
+                    _currentSelectedToggle.SetSelected(false);
+                    _lastSelectedToggle = _currentSelectedToggle;
+                }
+            }
+
+            _currentSelectedToggle = value;
+        }
+
+        public void DeselectEverything()
+        {
+            if (_currentSelectedToggle != null)
+            {
+                _lastSelectedToggle = _currentSelectedToggle;
+                _currentSelectedToggle = null;
+            }
+
+            for (int i = 0; i < _toggles.Count; i++)
+            {
+                _toggles[i].SetSelected(false);
+            }
+        }
+    }
+}
