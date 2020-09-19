739af1b41aa25e926843fdb91d5338d28bc213cf
Pierre Roy <lrp.roy@gmail.com>
2020-09-19 16:49:21 +0200

    singleton update

--- Singleton/Singleton.cs
+++ Singleton/Singleton.cs
@@ -5,9 +5,9 @@ namespace Tools
     public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
         /// Check to see if we're about to be destroyed.
-        private static bool m_ShuttingDown = false;
-        private static object m_Lock = new object();
-        private static T m_Instance;
+        private static bool _shuttingDown = false;
+        private static object _Lock = new object();
+        private static T _instance;
 
         /// <summary>
         /// Access singleton instance through this propriety.
@@ -16,25 +16,25 @@ namespace Tools
         {
             get
             {
-                if (m_ShuttingDown)
+                if (_shuttingDown)
                 {
                     Debug.LogWarning("Instance '" + typeof(T) + "' already destroyed.");
                     return null;
                 }
 
-                lock (m_Lock)
+                lock (_Lock)
                 {
-                    if (m_Instance == null)
+                    if (_instance == null)
                     {
                         /// Search for existing instance.
-                        m_Instance = (T)FindObjectOfType(typeof(T));
+                        _instance = (T)FindObjectOfType(typeof(T));
 
                         /// Create new instance if one doesn't already exist.
-                        if (m_Instance == null)
+                        if (_instance == null)
                         {
                             /// Need to create a new GameObject to attach the singleton to.
                             var singletonObject = new GameObject();
-                            m_Instance = singletonObject.AddComponent<T>();
+                            _instance = singletonObject.AddComponent<T>();
                             singletonObject.name = typeof(T).ToString() + " (Singleton)";
 
                             /// Make instance persistent.
@@ -42,19 +42,19 @@ namespace Tools
                         }
                     }
 
-                    return m_Instance;
+                    return _instance;
                 }
             }
         }
 
         private void OnApplicationQuit()
         {
-            m_ShuttingDown = true;
+            _shuttingDown = true;
         }
 
         private void OnDestroy()
         {
-            m_ShuttingDown = true;
+            _shuttingDown = true;
         }
     }
 }
