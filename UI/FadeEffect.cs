b148d1e285da0918cbbc2d47582ba776cf92f9a7
Pierre Roy <lrp.roy@gmail.com>
2021-02-06 16:59:19 +0100

    Random extensions + UI fade effect + abbreviation utility

---
+++ UI/FadeEffect.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Tools.UI
+{
+    public static class FadeEffect
+    {
+        public static IEnumerator FadeCanvas(CanvasGroup canvasGroup, float startAlpha, float endAlpha, float duration, bool unscaled = false)
+        {
+            float startTime = GetTime(unscaled);
+            float endTime = startTime + duration;
+            float elapsed = 0f;
+
+            canvasGroup.alpha = startAlpha;
+            canvasGroup.interactable = true;
+            canvasGroup.blocksRaycasts = true;
+
+            while (GetTime(unscaled) <= endTime)
+            {
+                elapsed = GetTime(unscaled) - startTime;
+                float percentage = 1 / (duration / elapsed);
+
+                // Fading out
+                if (startAlpha > endAlpha)
+                {
+                    canvasGroup.alpha = startAlpha - percentage;
+                }
+                // Fading in
+                else
+                {
+                    canvasGroup.alpha = startAlpha + percentage;
+                }
+
+                yield return new WaitForEndOfFrame();
+            }
+
+            canvasGroup.alpha = endAlpha;
+            canvasGroup.interactable = false;
+            canvasGroup.blocksRaycasts = false;
+        }
+
+        private static float GetTime(bool unscaled = false)
+        {
+            return unscaled ? Time.unscaledTime : Time.time;
+        }
+    }
+}
---
+++ Utilities/AbbreviationUtility.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Tools.Utilities
+{
+    public static class AbbreviationUtility
+    {
+        private static readonly SortedDictionary<long, string> abbrevations = new SortedDictionary<long, string>
+        {
+            {1000,"K"},
+            {1000000, "M" },
+            {1000000000, "B" },
+            {1000000000000,"T"}
+        };
+
+        public static string Format(float number, string format = "0.00")
+        {
+            for (int i = abbrevations.Count - 1; i >= 0; i--)
+            {
+                KeyValuePair<long, string> pair = abbrevations.ElementAt(i);
+
+                if (Mathf.Abs(number) >= pair.Key)
+                {
+                    return (number / pair.Key).ToString(format) + pair.Value;
+                }
+            }
+
+            return number.ToString();
+        }
+    }
+}
